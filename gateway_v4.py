2a2f93ea51353fbca1b1b67cd1625265da8d3348
Ginjo0815 <my.g.0815@gmail.com>
2024-04-09 18:42:09 +0900

    +

--- gateway_v4.py
+++ gateway_v4.py
@@ -2,7 +2,6 @@ import netifaces
 import subprocess
 import config
 
-
 def ping_gateway_v4():
     results = {}
     gateways = netifaces.gateways()
@@ -16,3 +15,27 @@ def ping_gateway_v4():
     large_packet_result = subprocess.run(large_packet_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     results['large'] = large_packet_result.returncode
     return results
+
+def display_gateway_v4():
+    title = "-------Gateway Ping Results-------\n"
+    results = ping_gateway_v4()
+    short_result = results['short']
+    large_result = results['large']
+
+    # 全体のステータスがOKかNGかを判断し、NGのみを表示
+    if short_result == 0 and large_result == 0:
+        overall_status_text = "OK"
+    else:
+        overall_status_text = "NG"
+
+    gateway_ip = netifaces.gateways()['default'][netifaces.AF_INET][0]
+    result_text = f"{title}{overall_status_text} (Short / Large) : {gateway_ip}\n\n"
+
+    return result_text,overall_status_text,short_result,large_result
+
+
+
+
+
+
+#ping_gateway_v4()
